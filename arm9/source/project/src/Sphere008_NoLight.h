b859234a5711c44020b15b0fb3b43cb2c79adb82
cotodevel <programmer.coto.1988@gmail.com>
2024-01-09 07:48:32 -0300

    update all TGDS Projects to TGDS1.65 commit: https://bitbucket.org/Coto88/toolchaingenericds/commits/3747fbc383933dd830a62b0c255d499c1a607f6f -fixed SnakeGL in TWL hardware, textures rendering properly, light, normals, vertices etc. Key input fixed as well.

---
+++ arm9/source/project/src/Sphere008_NoLight.h
@@ -0,0 +1,17 @@
+#ifndef __Sphere008_h__
+#define __Sphere008_h__
+
+#include "typedefsTGDS.h"
+
+#endif
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern u32 Sphere008_NoLight[];
+
+#ifdef __cplusplus
+}
+#endif
