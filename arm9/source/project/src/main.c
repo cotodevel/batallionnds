376cfefaf6463c60c64d251dbb4cb113f83980ab
cotodevel <programmer.coto.1988@gmail.com>
2024-04-05 18:05:06 -0300

    -rebuild TGDS-mb from scratch (newlib-nds + TGDS + TGDS-mb), reloads OK on TWL hardware + VRAM + DSWIFI -TGDS ARM7 VRAM Core: alloc 96K for program , 16K DLDI, 16K ARM7 malloc RAM -centralize vram loader bits -96K ARM7 VRAM Core fixes -implementation arm7bootldr ARM7 VRAM module (1/3) -implementation arm7bootldr ARM7 VRAM module (2/3) -implementation arm7bootldr ARM7 VRAM module (3/3) -TGDS Project now has proper DLDI section: 32K -TGDS DLDI code is now backported 100% to ARM code -ARM7DLDI is relocated properly as it was meant to be 3 years ago. -fix TGDS-MB v3 VRAM on NTR/TWL hardware on improved DLDI code -implementation TGDS ARM7 VRAM Core (1/2) -implementation TGDS ARM7 VRAM Core (2/2) -restore DSWNIFI in TGDS ARM7 VRAM core -synchronize proper ARM7DLDI init to executeARM7Payload(u32 arm7entryaddress, int arm7BootCodeSize, u32 * payload); -allow to merge existing TGDS ARM7 IWRAM Core bits into TGDS ARM7 VRAM Core

--- arm9/source/project/src/main.c
+++ arm9/source/project/src/main.c
@@ -108,6 +108,14 @@ extern int vsnprintf( char* buffer, size_t buf_size, const char* format, va_list
 #undef WIN32		// //
 #endif
 
+#ifdef ARM9
+#include "loader.h"
+
+//ARM7 VRAM core
+#include "arm7vram.h"
+#include "arm7vram_twl.h"
+#endif
+
 struct Scene scene;	/// the scene we render
 
 #if (defined(__GNUC__) && !defined(__clang__))
@@ -125,6 +133,20 @@ int main(int argc, char *argv[])
 
 	#ifdef ARM9
 	/*			TGDS 1.6 Standard ARM9 Init code start	*/
+	//Save Stage 1: IWRAM ARM7 payload: NTR/TWL (0x03800000)
+	memcpy((void *)TGDS_MB_V3_ARM7_STAGE1_ADDR, (const void *)0x02380000, (int)(96*1024));	//
+	coherent_user_range_by_size((uint32)TGDS_MB_V3_ARM7_STAGE1_ADDR, (int)(96*1024)); //		also for TWL binaries 
+	
+	//Execute Stage 2: VRAM ARM7 payload: NTR/TWL (0x06000000)
+	u32 * payload = NULL;
+	if(__dsimode == false){
+		payload = (u32*)&arm7vram[0];	
+	}
+	else{
+		payload = (u32*)&arm7vram_twl[0];
+	}
+	executeARM7Payload((u32)0x02380000, 96*1024, payload);
+	
 	bool isTGDSCustomConsole = true;	//set default console or custom console: custom console 
 	GUI_init(isTGDSCustomConsole);
 	GUI_clear();
